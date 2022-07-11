bb7aaed128605c462d3fe98971948678a5443567
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-07-10 20:31:46 -0700

    fix: switch to base64 encoded ssh key

--- tunnel.sh
+++ tunnel.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [[ -n "$BASTION_SSH_KEY" && -n "$DB_HOST" && -n "$BASTION_USERNAME" && -n "$BASTION_HOST" && -n "$BASTION_PORT" && -n "$DATABASE_PORT" ]];
+if [[ -n "$BASE64_ENCODED_BASTION_SSH_KEY" && -n "$DB_HOST" && -n "$BASTION_USERNAME" && -n "$BASTION_HOST" && -n "$BASTION_PORT" && -n "$DATABASE_PORT" ]];
 then
-    echo "$BASTION_SSH_KEY" > /flyway/bastion_ssh_private_key
+    echo -n "$BASE64_ENCODED_BASTION_SSH_KEY" | base64 -d > /flyway/bastion_ssh_private_key
     chmod 600 /flyway/bastion_ssh_private_key
     chown flyway:flyway /flyway/bastion_ssh_private_key
     ssh -o "StrictHostKeyChecking no" -i /flyway/bastion_ssh_private_key -f ${BASTION_USERNAME}@${BASTION_HOST} -p ${BASTION_PORT} -L ${DATABASE_PORT}:${DB_HOST}:${DATABASE_PORT} -N
     echo "Connected"
 else
-    echo "WARNING: BASTION_SSH_KEY, DB_HOST, BASTION_USERNAME, BASTION_HOST, BASTION_PORT, or DATABASE_PORT was not set. Note: BASTION HOST WILL NOT BE USED"
+    echo "WARNING: BASE64_ENCODED_BASTION_SSH_KEY, DB_HOST, BASTION_USERNAME, BASTION_HOST, BASTION_PORT, or DATABASE_PORT was not set. Note: BASTION HOST WILL NOT BE USED"
 fi
