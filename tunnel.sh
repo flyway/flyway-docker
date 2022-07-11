4987bae51eed1a5d4c030051786aca59e5ac4ac0
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-07-10 20:41:18 -0700

    feat: adding debugging output

--- tunnel.sh
+++ tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -n "$BASE64_ENCODED_BASTION_SSH_KEY" && -n "$DB_HOST" && -n "$BASTION_USERNAME" && -n "$BASTION_HOST" && -n "$BASTION_PORT" && -n "$DATABASE_PORT" ]];
-then
+then    
     echo -n "$BASE64_ENCODED_BASTION_SSH_KEY" | base64 -d > /flyway/bastion_ssh_private_key
     chmod 600 /flyway/bastion_ssh_private_key
     chown flyway:flyway /flyway/bastion_ssh_private_key
@@ -9,4 +9,9 @@ then
     echo "Connected"
 else
     echo "WARNING: BASE64_ENCODED_BASTION_SSH_KEY, DB_HOST, BASTION_USERNAME, BASTION_HOST, BASTION_PORT, or DATABASE_PORT was not set. Note: BASTION HOST WILL NOT BE USED"
+    echo "DB_HOST: ${DB_HOST}"
+    echo "BASTION_USERNAME: ${BASTION_USERNAME}"
+    echo "BASTION_HOST: ${BASTION_HOST}"
+    echo "BASTION_PORT: ${BASTION_PORT}"
+    echo "DATABASE_PORT: ${DATABASE_PORT}"
 fi
