21531c9a0048fd646d28ce37c9656b5feb1b1462
Tom Smith <tjsmith14599@outlook.com>
2024-04-03 17:36:37 +0100

    await and download artifacts using python

---
+++ download_artifact.py
@@ -0,0 +1,32 @@
+import sys
+import urllib.request
+from urllib.error import HTTPError
+import time
+
+
+def get_repo_url(edition, version):
+    if edition == "oss":
+        return f'https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/{version}/flyway-commandline-{version}.tar.gz'
+    if edition == "redgate":
+        return f'https://download.red-gate.com/maven/release/org/flywaydb/enterprise/flyway-commandline/{version}/flyway-commandline-{version}.tar.gz'
+    print("Edition should be 'oss' or 'redgate'")
+    exit(1)
+    
+
+if __name__ == "__main__":
+    edition = sys.argv[1]
+    version = sys.argv[2]
+    url = get_repo_url(edition, version)
+    
+    while True:
+        try:
+            response = urllib.request.urlopen(url)
+            artifact_file = open(f'flyway-commandline-{version}.tar.gz', 'wb')
+            artifact_file.write(response.read())
+            response.close()
+            artifact_file.close()
+            break
+        except HTTPError:
+            time.sleep(2)
+            print("Unable to download artifacts. Trying again in 2 seconds...")
+    
