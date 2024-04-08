6f1a0d86039608e60339f4ab6e3d996337cee20f
Tom Smith <tjsmith14599@outlook.com>
2024-04-08 13:55:10 +0100

    fail if subprocess fails

--- scripts/build_images.py
+++ scripts/build_images.py
@@ -45,4 +45,4 @@ if __name__ == "__main__":
     
     for command in commands:
         print(f'Running docker build command: {command}')
-        subprocess.run(command)
+        subprocess.run(command, check=True)
--- scripts/test_images.py
+++ scripts/test_images.py
@@ -5,7 +5,8 @@ import subprocess
 
 def build_non_multi_arch_standard_flyway_image(version):
     build_command = f'docker build --target flyway --pull --build-arg FLYWAY_VERSION={version} -q -f ./dockerfiles/Dockerfile .'
-    return subprocess.run(build_command, capture_output=True, encoding="UTF_8").stdout.strip()
+    print(build_command)
+    return subprocess.run(build_command, capture_output=True, encoding="UTF_8", check=True).stdout.strip()
     
 
 if __name__ == "__main__":
@@ -32,7 +33,5 @@ if __name__ == "__main__":
         for flyway_command in flyway_commands:
             run_command = f'docker run --rm -v "{test_sql_path}:/flyway/sql" {image} {flyway} {flyway_command} {flyway_cli_params}'
             print(run_command)
-            result = subprocess.run(run_command)
-            if result.returncode != 0:
-                exit(result.returncode)
+            subprocess.run(run_command, check=True)
         
