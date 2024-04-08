33320dd48ddb746470ee2f1cf4f631e124e2da3e
Tom Smith <tjsmith14599@outlook.com>
2024-04-08 13:55:36 +0100

    allow optional environment variable to be passed into docker container

--- scripts/test_images.py
+++ scripts/test_images.py
@@ -20,7 +20,11 @@ if __name__ == "__main__":
         images[0] = build_non_multi_arch_standard_flyway_image(version)
     
     test_sql_path = os.getcwd() + "/test-sql"
-    
+
+    env_var_flag = ""
+    if len(sys.argv) > 3:
+        env_var_flag = f'-e {sys.argv[3]}={os.getenv(sys.argv[3])}'
+
     flyway_cli_params = "-url=jdbc:h2:mem:test "
     if edition == "redgate":
         flyway_cli_params += f'-licenseKey={os.environ["FLYWAY_LICENSE_KEY"]} '
@@ -31,7 +35,7 @@ if __name__ == "__main__":
         else:
             flyway = ""
         for flyway_command in flyway_commands:
-            run_command = f'docker run --rm -v "{test_sql_path}:/flyway/sql" {image} {flyway} {flyway_command} {flyway_cli_params}'
+            run_command = f'docker run --rm -v "{test_sql_path}:/flyway/sql" {env_var_flag} {image} {flyway} {flyway_command} {flyway_cli_params}'
             print(run_command)
             subprocess.run(run_command, check=True)
         
