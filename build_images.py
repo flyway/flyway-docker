83e067d808ac44e3f1a74bdcef391d532ba6620f
Tom Smith <tjsmith14599@outlook.com>
2024-04-08 11:48:45 +0100

    move all dockerfiles into separate folder

--- build_images.py
+++ build_images.py
@@ -22,13 +22,14 @@ def get_buildx_command(edition, platforms, version, tag_suffix, folder, push=Fal
         pull_or_push = "push"
     command = f'docker buildx build --target {edition} --platform {platforms} --{pull_or_push} --build-arg FLYWAY_VERSION={version} '
     command += get_tag_flags(version, edition, tag_suffix)
-    return command + folder
+    file_flag = f'-f ./dockerfiles/{folder}/Dockerfile '
+    return command + file_flag + folder
     
     
 def get_build_command(edition, version, tag_suffix, folder):
     command = f'docker build --target {edition} --pull --build-arg FLYWAY_VERSION={version} '
     command += get_tag_flags(version, edition, tag_suffix)
-    file_flag = f'-f {folder}/Dockerfile '
+    file_flag = f'-f ./dockerfiles/{folder}/Dockerfile '
     return command + file_flag + "."
     
 
@@ -44,8 +45,8 @@ if __name__ == "__main__":
         commands.append(get_buildx_command(edition, "linux/arm/v7,linux/arm64/v8,linux/amd64", version, "", "."))
     else:
         commands.append(get_build_command(edition, version, "", "."))
-    commands.append(get_build_command(edition, version, "-alpine", "./alpine"))
-    commands.append(get_build_command(edition, version, "-azure", "./azure"))
+    commands.append(get_build_command(edition, version, "-alpine", "alpine"))
+    commands.append(get_build_command(edition, version, "-azure", "azure"))
     
     for command in commands:
         print(f'Running docker build command: {command}')
