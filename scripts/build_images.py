714d4cf5a581c0a51f014c94a61df0be9a663513
WilliamL-RG <william.liversidge@red-gate.com>
2024-12-11 11:59:44 +0000

    Create separate Mongo images

--- scripts/build_images.py
+++ scripts/build_images.py
@@ -3,28 +3,43 @@ import utils
 import subprocess
 
 
-def get_tag_flags(version, edition, tag_suffix):
+def get_tag_flags(version, edition, tag_suffix, mongo=False):
+    qualifier = ""
+    if mongo:
+        qualifier = "-mongo"
     tags = utils.generate_tags(version, tag_suffix)
     command_flags = ""
     for tag in tags:
-        command_flags += f'-t {edition}/flyway:{tag} '
+        command_flags += f'-t {edition}/flyway:{tag}{qualifier} '
     return command_flags
 
 
-def get_buildx_command(edition, version, tag_suffix, folder, push=False):
+def get_buildx_command(edition, version, tag_suffix, folder, mongo=False, push=False):
     pull_or_push = "pull"
     if push:
         pull_or_push = "push"
-    command = f'docker buildx build --target {edition} --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --{pull_or_push} --build-arg FLYWAY_VERSION={version} '
-    command += get_tag_flags(version, edition, tag_suffix)
-    file_flag = f'-f ./dockerfiles/{folder}/Dockerfile '
+    platforms = "linux/arm/v7,linux/arm64/v8,linux/amd64"
+    dockerfile = "Dockerfile"
+    target = f"--target {edition}"
+    if mongo:
+        platforms = "linux/arm64/v8,linux/amd64"
+        dockerfile = "Dockerfile-mongo"
+        target = ""
+    command = f'docker buildx build {target} --platform {platforms} --{pull_or_push} --build-arg FLYWAY_VERSION={version} '
+    command += get_tag_flags(version, edition, tag_suffix, mongo)
+    file_flag = f'-f ./dockerfiles/{folder}/{dockerfile} '
     return command + file_flag + folder
     
     
-def get_build_command(edition, version, tag_suffix, folder):
-    command = f'docker build --target {edition} --pull --build-arg FLYWAY_VERSION={version} '
-    command += get_tag_flags(version, edition, tag_suffix)
-    file_flag = f'-f ./dockerfiles/{folder}/Dockerfile '
+def get_build_command(edition, version, tag_suffix, folder, mongo=False):
+    dockerfile = "Dockerfile"
+    target = f"--target {edition}"
+    if mongo:
+        dockerfile = "Dockerfile-mongo"
+        target = ""
+    command = f'docker build {target} --pull --build-arg FLYWAY_VERSION={version} '
+    command += get_tag_flags(version, edition, tag_suffix, mongo)
+    file_flag = f'-f ./dockerfiles/{folder}/{dockerfile} '
     return command + file_flag + "."
     
 
@@ -38,10 +53,15 @@ if __name__ == "__main__":
         commands.append("docker run --rm --privileged multiarch/qemu-user-static --reset -p yes")
         commands.append("docker buildx create --name multi_arch_builder --driver docker-container --driver-opt network=bridge --use")
         commands.append(get_buildx_command(edition, version, "", "."))
+        commands.append(get_buildx_command(edition, version, "", ".", True))
+        commands.append(get_build_command(edition, version, "-alpine", "alpine"))
+        commands.append(get_build_command(edition, version, "-azure", "azure"))
+        commands.append(get_build_command(edition, version, "-alpine", "alpine", True))
+        commands.append(get_build_command(edition, version, "-azure", "azure", True))
     else:
         commands.append(get_build_command(edition, version, "", "."))
-    commands.append(get_build_command(edition, version, "-alpine", "alpine"))
-    commands.append(get_build_command(edition, version, "-azure", "azure"))
+        commands.append(get_build_command(edition, version, "-alpine", "alpine"))
+        commands.append(get_build_command(edition, version, "-azure", "azure"))
     
     for command in commands:
         print(f'Running docker build command: {command}')
