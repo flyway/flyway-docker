415d80a4b8e9e00c06301f43a6baafc2f4780432
JasonLuo-Redgate <130552403+JasonLuo-Redgate@users.noreply.github.com>
2024-12-12 11:35:18 +0000

    add mongo image to the release

--- scripts/release.py
+++ scripts/release.py
@@ -16,9 +16,12 @@ if __name__ == "__main__":
     tags = []
     if edition == "flyway":
         # Multi-arch images are pushed using the buildx command
-        release_commands.append(get_buildx_command(edition, version, "", ".", True))
+        release_commands.append(get_buildx_command(edition, version, "", ".", False, True))
         tags.extend(utils.generate_tags(version, "-alpine"))
         tags.extend(utils.generate_tags(version, "-azure"))
+        release_commands.append(get_buildx_command(edition, version, "", ".", True, True))
+        tags.extend(utils.generate_tags(version, "-alpine-mongo"))
+        tags.extend(utils.generate_tags(version, "-azure-mongo"))
     else:
         tags.extend(utils.generate_tags(version, ""))
         tags.extend(utils.generate_tags(version, "-alpine"))
